7a975c9ea548cf82e0ce3fa33619d064961e0a27
Pandog <rndlr96@gmail.com>
2018-04-13 00:11:43 +0900

    Update Function
    Update Function ( Create, Remove, Edit )

--- src/Contact.js
+++ src/Contact.js
@@ -25,6 +25,36 @@ export default class Contact extends React.Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+  }
+
+  handleCreate(contact){
+    this.setState({
+      contactData: update(this.state.contactData, {$push: [contact]})
+    });
+  }
+
+  handleRemove(){
+    this.setState({
+      contactData: update(this.state.contactData,
+          { $splice : [[this.state.selectedKey, 1]]}
+      ),
+      selectedKey: -1
+    })
+  }
+
+  handleEdit(name, phone){
+    this.setState({
+      contactData: update(this.state.contactData,
+      {
+        [this.state.selectedKey]: {
+          name: { $set: name },
+          phone: { $set: phone }
+        }
+      })
+    })
   }
 
   handleChange(e){
